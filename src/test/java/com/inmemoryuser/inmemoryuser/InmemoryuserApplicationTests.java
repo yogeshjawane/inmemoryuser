954ae0eb4499aaf095d2d562e51f36852a46e592
yogeshjawane <jawane_yogesh@yahoo.com>
2022-02-19 14:16:30 +0530

    Initial commit

---
+++ src/test/java/com/inmemoryuser/inmemoryuser/InmemoryuserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inmemoryuser.inmemoryuser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InmemoryuserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
